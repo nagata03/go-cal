d656f9c79d59116869335805189d588a4a1667af
nagata03 <nagata.job@gmail.com>
2019-06-16 16:32:43 +0900

    カレンダー表示処理を関数に切り出し

--- main.go
+++ main.go
@@ -6,27 +6,13 @@ import (
 	"time"
 )
 
-func main() {
-	t := time.Now() // 現在の時刻を取得 tはtime.Time型
-
-	// 今月初日の曜日を取得する
-	firstDay := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, time.Local)
-	wday := firstDay.Weekday()
-
-	// 今月の最終日を取得する
-	lastDay := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, -1)
-
-	//
-	// カレンダーを表示する
-	//
-	fmt.Printf("     %v %d\n", t.Month(), t.Year()) // 月と年を表示
+func printCal(today time.Time, wday int, days int) {
+	fmt.Printf("     %v %d\n", today.Month(), today.Year()) // 月と年を表示
 	fmt.Println("Su Mo Tu We Th Fr Sa")             // 曜日を表示
-
-	// 1日までのスペースを表示
-	fmt.Print(strings.Repeat("   ", int(wday)))
+	fmt.Print(strings.Repeat("   ", int(wday)))     // 1日までのスペースを表示
 
 	// 日付を表示
-	for day := 1; day <= lastDay.Day(); day++ {
+	for day := 1; day <= days; day++ {
 		fmt.Printf("%2d ", day)
 		wday++
 		if wday == 7 {
@@ -34,4 +20,13 @@ func main() {
 			wday = 0
 		}
 	}
+	fmt.Println("")
+}
+
+func main() {
+	t := time.Now() // 現在の時刻を取得 tはtime.Time型
+	wday := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, time.Local).Weekday()  // 今月初日の曜日を取得する
+	days := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, -1).Day()  // 今月の最終日を取得する
+
+	printCal(t, int(wday), days)
 }
