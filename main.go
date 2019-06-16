0173e3949a8e5f42f84df88d518fc3e11d099f14
nagata03 <nagata.job@gmail.com>
2019-06-16 18:38:16 +0900

    コメント修正

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func printCal(today time.Time, wday int, days int) {
 	fmt.Printf("     %v %d\n", today.Month(), today.Year()) // 月と年を表示
-	fmt.Println("Su Mo Tu We Th Fr Sa")             // 曜日を表示
-	fmt.Print(strings.Repeat("   ", int(wday)))     // 1日までのスペースを表示
+	fmt.Println("Su Mo Tu We Th Fr Sa")                     // 曜日を表示
+	fmt.Print(strings.Repeat("   ", int(wday)))             // 1日までのスペースを表示
 
 	// 日付を表示
 	for day := 1; day <= days; day++ {
@@ -24,9 +24,9 @@ func printCal(today time.Time, wday int, days int) {
 }
 
 func main() {
-	t := time.Now() // 現在の時刻を取得 tはtime.Time型
-	wday := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, time.Local).Weekday()  // 今月初日の曜日を取得する
-	days := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, -1).Day()  // 今月の最終日を取得する
+	t := time.Now()                                                                             // 現在の日付・時刻を取得 tはtime.Time型
+	wday := time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, time.Local).Weekday()                 // 今月初日の曜日を取得
+	days := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, -1).Day() // 今月最終日の日付（=今月の日数）を取得
 
 	printCal(t, int(wday), days)
 }
